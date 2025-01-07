d52c858b61364bd31226083540dc31ef3036d3aa
Kiersna <kiersnowskid@gmail.com>
2025-01-07 23:35:09 +0100

    update

--- ZadanieBibliotekaOOP/Program.cs
+++ ZadanieBibliotekaOOP/Program.cs
@@ -1,8 +1,9 @@
 ﻿using static Program;
-//teraz robie logowanie i rejestracje i probuje sprawdzic czy uzytrkonwik naprawde istneije w momencie logowania 
+//logowanie zeby chodzilo poprawnie 
+//ogaranc statici i nie statici czy sa w dobrych miejscach itp i jak jest lepiej
 public class Program
 {
-    
+
     public abstract class ElementBiblioteki
     {
         public string? Tytul { get; set; }
@@ -60,22 +61,22 @@ public class Program
             Console.WriteLine("Imie: ");
             string imie = Console.ReadLine();
             Uzytkownik nowy = new Uzytkownik(login, imie);
-            Console.WriteLine($"Zarejestrowano uzytkownika o imieniu {nowy.imie} z loginem {nowy.login}");        
+            Console.WriteLine($"Zarejestrowano uzytkownika o imieniu {nowy.imie} z loginem {nowy.login}");
         }
-        public static void Zaloguj()
+        public static bool Zaloguj()
         {
             Console.WriteLine("Podaj login:");
             string login = Console.ReadLine();
             if (uzytkownicy.Exists(u => u.login == login))
             {
-                menuPoZalogowaniu();
-            }
+                return true;
+            }return false;
         }
     }
     public interface IOperacjeBiblioteczne
     {
-        public void Wypozycz(Ksiazka ksiazka);
-        public void Zwroc(Ksiazka ksiazka);
+        public void Wypozycz(string tytul);
+        public void Zwroc(string tytul);
     }
     public class Biblioteka : IOperacjeBiblioteczne
     {
@@ -116,36 +117,60 @@ public class Program
             });
         }
 
-            public static void WyswietlKsiazki()
+        public static void WyswietlKsiazki()
         {
-            foreach(Ksiazka k in ksiazki)
+            foreach (Ksiazka k in ksiazki)
             {
                 Console.WriteLine(k.Tytul);
             }
         }
-            public void Wypozycz(Ksiazka ksiazka)
+        public void Wypozycz(string tytul)
+        {
+            var ksiazka = ksiazki.FirstOrDefault(k => k.Tytul.Equals(tytul, StringComparison.OrdinalIgnoreCase));
+            if (ksiazka == null)
             {
-            if (ksiazka.Status == true)
+                Console.WriteLine("Nie odnaleziono książki o podanym tytule.");
+                return;
+            }
+            if (ksiazka.Status)
             {
                 ksiazka.ZmienStatus(false);
-                Console.WriteLine("Ksiazka zostala wypozyczona");
+                Console.WriteLine($"Ksiazka {ksiazka} zostala wypozyczona");
             }
             else
             {
-                Console.WriteLine("Ksiazka jest juz wypozyczona");
+                Console.WriteLine($"Ksiazka {ksiazka} jest juz wypozyczona");
             }
         }
-        public void Zwroc(Ksiazka ksiazka)
+        public void Zwroc(string tytul)
         {
-            ksiazka.ZmienStatus(true);
-            Console.WriteLine("Ksiazka zostala zwrocona");
+            var ksiazka = ksiazki.FirstOrDefault(k => k.Tytul.Equals(tytul, StringComparison.OrdinalIgnoreCase));
+            if (ksiazka == null)
+            {
+                Console.WriteLine("Nie odnaleziono książki o podanym tytule.");
+                return;
+            }
+            if (!ksiazka.Status)
+            {
+                ksiazka.ZmienStatus(true);
+                Console.WriteLine($"Ksiazka {ksiazka} zostala zwrocona");
+            }
+            else
+            {
+                Console.WriteLine($"Ksiazka {ksiazka} nie byla wypozyczona");
+            }
+            //czyli akutalnie program zaklada ze biblioteka bedzie miala tylko 1 egzemplarz ksiazki
+            //i nie ma sprawdzania czy oddaje ja ta sama osoba co wypozyczyla
         }
     }
-    public void menu()
+
+    public static void Main()
     {
+        int wybor = -1;
+        while (wybor != 0) { 
         Console.WriteLine("1. Zarejestuj");
         Console.WriteLine("2. Zaloguj");
-        int wybor = -1;
+            Console.WriteLine("0. Zakoncz program");
         try
         {
             wybor = Int32.Parse(Console.ReadLine());
@@ -154,44 +179,64 @@ public class Program
         {
             Console.WriteLine("Nieprawidłowa wartość");
         }
-        switch (wybor){
-            case 1:
-                Uzytkownik.Zarejestruj();
-                break;
-            case 2:
-                Console.WriteLine("Podaj Login: ");
-                break;
-               
-
+            switch (wybor)
+            {
+                case 1:
+                    Uzytkownik.Zarejestruj();
+                    break;
+                case 2:
+                    if (Uzytkownik.Zaloguj())
+                    {
+                        menuPoZalogowaniu();
+                        wybor = 0;
+                    }
+                    break;
+                case 0:
+                    return;
+            }
         }
     }
     public static void menuPoZalogowaniu()
     {
-        Console.WriteLine("1. Wyswietl ksiazki");
-        Console.WriteLine("2. Wypozycz ksiazki");
-        Console.WriteLine("3. Zwroc ksiazki");
-        int wybor = -1;
-        try
+        var biblioteka = new Biblioteka();
+        while (true)
         {
-            wybor = Int32.Parse(Console.ReadLine());
-        }
-        catch
-        {
-            Console.WriteLine("Nieprawidłowa wartość");
-        }
-        switch (wybor)
-        {
-            case 1:
-                Biblioteka.WyswietlKsiazki();
-                break;
-            case 2:
-                Biblioteka.Wypozycz(ksiazka);
-                break;
-            case 3:
-                Biblioteka.Zwroc(ksiazka);
-                break;
-
-
+            Console.WriteLine("1. Wyswietl ksiazki");
+            Console.WriteLine("2. Wypozycz ksiazki");
+            Console.WriteLine("3. Zwroc ksiazki");
+            Console.WriteLine("4. Wyjdź");
+            Console.WriteLine("Wybierz opcje: ");
+            int wybor = -1;
+            try
+            {
+                wybor = Int32.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Nieprawidłowa wartość");
+            }
+            switch (wybor)
+            {
+                case 1:
+                    Biblioteka.WyswietlKsiazki();
+                    break;
+                case 2:
+                    Console.WriteLine("Podaj tytuł książki do wypożyczenia: ");
+                    string tytulWypozycz = Console.ReadLine();
+                    biblioteka.Wypozycz(tytulWypozycz);
+                    break;
+                case 3:
+                    Console.WriteLine("Podaj tytuł książki do zwrotu: ");
+                    string tytulZwrot = Console.ReadLine();
+                    biblioteka.Zwroc(tytulZwrot);
+                    break;
+                case 4:
+                    Console.WriteLine("Zakonczono program");
+                    return;
+                default:
+                    Console.WriteLine("Niepoprawny wybór");
+                    break;
+            }
         }
     }
 }
